2586607a121082fdc456e22da687a948b5d905ce
Josh Clifford <37558619+jsclifford@users.noreply.github.com>
2024-10-03 13:39:57 -0600

    SRE-208 - Updated packages, storageclient, and codeowners.

--- src/StorageClient.ts
+++ src/StorageClient.ts
@@ -1,4 +1,4 @@
-import * as azStorage from "azure-storage";
+import { BlobServiceClient } from "@azure/storage-blob";
 import { SharedOptions } from "./SharedOptions";
 
 export interface StorageClientOptions extends SharedOptions {
@@ -6,45 +6,50 @@ export interface StorageClientOptions extends SharedOptions {
 }
 
 export class StorageClient {
-  private blobService?: azStorage.BlobService;
+  private blobService?:BlobServiceClient;
 
   public async uploadLocalFile(containerName: string, blobName: string, localFileName: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      this.blobService!.createBlockBlobFromLocalFile(containerName, blobName, localFileName, (err, result, response) => {
-        if (err) {
-          this.options.error("Failed to upload file:", blobName, "Error:", err);
-          reject(err);
-        } else {
+      const containerClient = this.blobService!.getContainerClient(containerName);
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+      const response = blockBlobClient!.uploadFile(localFileName);
+      response.then((result) => {
+        if (result._response.status == 200) {
           let fileLink = this.hostName() + containerName + "/" + blobName;
           this.options.log("Uploaded file", fileLink);
           resolve(fileLink);
-        }
+        } 
+      }).catch((err) => {
+        this.options.error("Failed to upload file:", blobName, "Error:", err);
+        reject(err);
       });
     });
   }
 
   public async ensureContainerExists(containerName: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      this.blobService!.createContainerIfNotExists(containerName, {}, (err, result, response) => {
-        if (err) {
-          console.error("Failed to create container", containerName, "Error:", err);
-          reject(err);
-        } else {
+      const containerClient = this.blobService!.getContainerClient(containerName);
+      const response = containerClient!.createIfNotExists();
+      response.then((result) => {
+        if (result.succeeded) {
           resolve(this.hostName() + containerName);
-        }
+        } 
+      }).catch((err) => {
+        this.options.error("Failed to create container", containerName, "Error:", err);
+        reject(err);
       });
     });
   }
 
   constructor(private options: StorageClientOptions) {
     if (options.connectionString != "") {
-      this.blobService = azStorage.createBlobService(options.connectionString);
+      this.blobService = new BlobServiceClient(options.connectionString);
     } else {
       this.blobService = undefined;
     }
   }
 
   public hostName(): string {
-    return this.blobService!.host.primaryHost;
+    return this.blobService!.url;
   }
 }
